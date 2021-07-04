230351f97eacb2daac0b08a9f56d75eb87e25142
mohammad h alijany <mh.alijany@gmail.com>
2021-07-05 03:59:28 +0430

    fix file souce super type

--- main/contract/utilities/FileSource.ts
+++ main/contract/utilities/FileSource.ts
@@ -1,4 +1,9 @@
+import { PathLike } from "original-fs";
 import DataSource from "./DataSource";
 
-export default abstract class FileSource 
-extends DataSource<Promise<Buffer>, string | Buffer> {}
+export default abstract class FileSource
+    extends DataSource<Promise<Buffer>, string | Buffer> {
+
+    public abstract setPath(path: PathLike): void;
+
+}
--- main/contract/utilities/JsonFileSourceDecorator.ts
+++ main/contract/utilities/JsonFileSourceDecorator.ts
@@ -1,10 +1,13 @@
+import DataSourceDecorator from "./DataSourceDecorator";
 import FileSource from "./FileSource";
 
-export default abstract class JsonFileSourceDecorator extends FileSource {
+export default abstract class JsonFileSourceDecorator
+    extends DataSourceDecorator<Promise<any>, any> {
 
     public abstract setDataSource(dataSource: FileSource): void;
 
     public abstract write(data: any): Promise<void> | void;
 
-    public abstract read(): any
+    public abstract read(): any;
+    
 }
