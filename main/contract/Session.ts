a8f8e4ff9a740db336d64dd272c33ebde64d4df9
mohammad h alijany <mh.alijany@gmail.com>
2021-07-08 16:32:47 +0430

    fix type

--- main/contract/Session.ts
+++ main/contract/Session.ts
@@ -7,7 +7,7 @@ export default abstract class Session<Type> {
     public abstract has(key: string): boolean;
 
     // Get an item from the session.
-    public abstract get(key: string, $default: Type): Type;
+    public abstract get(key: string, $default?: Type): Type | undefined;
 
     // Put a value in the session and return its key.
     public abstract add(value: Type): string;
